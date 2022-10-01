72aa7c432bc59819d07393bf91623fa963896ebe
Luc Georges <luc@huggingface.co>
2022-10-01 11:32:34 +0200

    feat: add -g globals flag

--- src/picklescan/cli.py
+++ src/picklescan/cli.py
@@ -3,7 +3,7 @@ import logging
 import os
 import sys
 
-from .scanner import scan_directory_path
+from .scanner import SafetyLevel, scan_directory_path
 from .scanner import scan_file_path
 from .scanner import scan_url
 from .scanner import scan_huggingface_model
@@ -19,9 +19,12 @@ def main():
     group.add_argument("-p", "--path", help="Path to the file or folder to scan", dest='path')
     group.add_argument("-u", "--url", help="URL to the file or folder to scan", dest='url')
     group.add_argument("-hf", "--huggingface", help="Name of the Hugging Face model to scan", dest='huggingface_model')
+    parser.add_argument("-g", "--globals", help="list all globals found", action='store_true')
+    parser.set_defaults(globals=False)
     parser.add_argument(
         "-l", "--log", help="level of log messages to display (default: INFO)", dest='log_level',
         choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'])
+
 
     args = parser.parse_args()
 
@@ -34,21 +37,26 @@ def main():
             if not os.path.exists(path):
                 raise FileNotFoundError(f"Path {path} does not exist")
             if os.path.isdir(path):
-                files_scanned_count, files_suspicious_count = scan_directory_path(path)
+                scan_result = scan_directory_path(path)
             else:
-                files_scanned_count, files_suspicious_count = scan_file_path(path)
+                scan_result = scan_file_path(path)
         elif args.url is not None:
-            files_scanned_count, files_suspicious_count = scan_url(args.url)
+            scan_result = scan_url(args.url)
         elif args.huggingface_model is not None:
-            files_scanned_count, files_suspicious_count = scan_huggingface_model(args.huggingface_model)
+            scan_result = scan_huggingface_model(args.huggingface_model)
         else:
             raise ValueError("Command line must include either a path, a URL, or a Hugging Face model")
 
         _log.info(f"""----------- SCAN SUMMARY -----------
-Scanned files: {files_scanned_count}
-Infected files: {files_suspicious_count}""")
+Scanned files: {scan_result.scanned_files}
+Infected files: {scan_result.infected_files}
+# of {SafetyLevel.Dangerous.value} globals: {scan_result.issues_count}""")
+        if args.globals and len(scan_result.globals) > 0:
+            _log.info("All globals found:")
+            for g in scan_result.globals:
+                _log.info(f"  * {g.module}.{g.name} - {g.safety.value}")
 
-        return 0 if files_suspicious_count == 0 else 1
+        return 0 if scan_result.issues_count == 0 else 1
     except Exception:
         _log.exception("Unhandled exception")
         return 2
--- src/picklescan/scanner.py
+++ src/picklescan/scanner.py
@@ -1,13 +1,49 @@
+from dataclasses import dataclass
 import http.client
 import io
 import json
 import logging
+from enum import Enum
 import os
 import pickletools
+from typing import List, Set, Tuple
 import urllib.parse
 import zipfile
 
+class SafetyLevel(Enum):
+    Innocuous = "innocuous"
+    Suspicious = "suspicious"
+    Dangerous = "dangerous"
+
+
+@dataclass
+class Global:
+    module: str
+    name: str
+    safety: SafetyLevel
+
+
+@dataclass
+class ScanResult:
+    globals: List[Global]
+    scanned_files: int = 0
+    issues_count: int = 0
+    infected_files: int = 0
+
+    def merge(self, sr: "ScanResult"):
+        self.globals.extend(sr.globals)
+        self.scanned_files += sr.scanned_files
+        self.issues_count += sr.issues_count
+        self.infected_files += sr.infected_files
+
+
 _log = logging.getLogger("picklescan")
+
+_safe_globals = {
+        "collections": {"OrderedDict"},
+        "torch": {"LongStorage", "FloatStorage", "HalfStorage", "QUInt2x4Storage", "QUInt4x2Storage", "QInt32Storage", "QInt8Storage", "QUInt8Storage", "ComplexFloatStorage", "ComplexDoubleStorage", "DoubleStorage", "BFloat16Storage", "BoolStorage", "CharStorage", "ShortStorage", "IntStorage", "ByteStorage"},
+        "torch._utils": {"_rebuild_tensor_v2"},
+}
 
 _suspicious_globals = {
     "__builtin__": {"eval", "compile", "getattr", "apply", "exec", "open", "breakpoint"},  # Pickle versions 0, 1, 2 have those function under '__builtin__'
@@ -45,8 +81,10 @@ _suspicious_globals = {
 # https://www.tensorflow.org/api_docs/python/tf/keras/models/load_model
 #
 
+_joblib_file_extensions = {".joblib"}
+_pytorch_file_extensions = {".bin", ".pt", ".pth", ".ckpt"}
 _pickle_file_extensions = {".pkl", ".pickle", ".joblib"}
-_zip_file_extensions = {".zip", ".bin"}  # PyTorch's pytorch_model.bin are zip archives
+_zip_file_extensions = {".zip", ".bin", ".npz"}  # PyTorch's pytorch_model.bin are zip archives
 
 
 def _http_get(url):
@@ -69,7 +107,7 @@ def _http_get(url):
         conn.close()
 
 
-def _list_globals(data):
+def _list_globals(data) -> Set[Tuple[str, str]]:
 
     globals = set()
 
@@ -110,24 +148,33 @@ def _list_globals(data):
     return globals
 
 
-def scan_pickle_bytes(data, file_id):
+def scan_pickle_bytes(data, file_id) -> ScanResult:
     """Disassemble a Pickle stream and report issues"""
 
-    globals = _list_globals(data)
-    _log.debug("Global imports in %s: %s", file_id, globals)
+    globals = []
+    raw_globals = _list_globals(data)
+    _log.debug("Global imports in %s: %s", file_id, raw_globals)
 
     issues_count = 0
-    for g in globals:
-        filter = _suspicious_globals.get(g[0])
-        if filter is not None and (filter == "*" or g[1] in filter):
-            _log.warning("%s: global import '%s %s' FOUND", file_id, g[0], g[1])
+    for rg in raw_globals:
+        g = Global(rg[0], rg[1], SafetyLevel.Dangerous)
+        safe_filter = _safe_globals.get(g.module)
+        danger_filter = _suspicious_globals.get(g.module)
+        if danger_filter is not None and (danger_filter == "*" or g.name in danger_filter):
+            g.safety = SafetyLevel.Dangerous
+            _log.warning("%s: %s import '%s %s' FOUND", file_id, g.safety.value, g.module, g.name)
             issues_count += 1
+        elif safe_filter is not None and (safe_filter == "*" or g.name in safe_filter):
+            g.safety = SafetyLevel.Innocuous
+        else:
+            g.safety = SafetyLevel.Suspicious
+        globals.append(g)
 
-    return issues_count
+    return ScanResult(globals, 1, issues_count, 1 if issues_count > 0 else 0)
 
 
-def scan_zip_bytes(data, file_id):
-    issues_count = 0
+def scan_zip_bytes(data, file_id) -> ScanResult:
+    result = ScanResult([])
 
     with zipfile.ZipFile(io.BytesIO(data), "r") as zip:
         file_names = zip.namelist()
@@ -136,12 +183,12 @@ def scan_zip_bytes(data, file_id):
             if os.path.splitext(file_name)[1] in _pickle_file_extensions:
                 _log.debug("Scanning file %s in zip archive %s", file_name, file_id)
                 with zip.open(file_name, "r") as file:
-                    issues_count += scan_pickle_bytes(file.read(), f"{file_id}:{file_name}")
+                    result.merge(scan_pickle_bytes(file.read(), f"{file_id}:{file_name}"))
 
-    return issues_count
+    return result
 
 
-def scan_bytes(data, file_id):
+def scan_bytes(data, file_id) -> ScanResult:
     return scan_zip_bytes(data, file_id) if zipfile.is_zipfile(io.BytesIO(data)) else scan_pickle_bytes(data, file_id)
 
 
@@ -151,24 +198,20 @@ def scan_huggingface_model(repo_id):
     file_names = [file_name for file_name in (sibling.get("rfilename") for sibling in model["siblings"]) if file_name is not None]
 
     # Scan model files
-    files_scanned_count = 0
-    files_suspicious_count = 0
+    scan_result = ScanResult([])
     for file_name in file_names:
         file_ext = os.path.splitext(file_name)[1]
         if file_ext not in _zip_file_extensions and file_ext not in _pickle_file_extensions:
             continue
         _log.debug("Scanning file %s in model %s", file_name, repo_id)
         url = f"https://huggingface.co/{repo_id}/resolve/main/{file_name}"
-        issues_count = scan_bytes(_http_get(url), url)
-        files_suspicious_count += 1 if issues_count > 0 else 0
-        files_scanned_count += 1
+        scan_result.merge(scan_bytes(_http_get(url), url))
 
-    return files_scanned_count, files_suspicious_count
+    return scan_result
 
 
-def scan_directory_path(path):
-    files_scanned_count = 0
-    files_suspicious_count = 0
+def scan_directory_path(path) -> ScanResult:
+    scan_result = ScanResult([])
 
     for base_path, _, file_names in os.walk(path):
         for file_name in file_names:
@@ -179,21 +222,17 @@ def scan_directory_path(path):
             _log.debug("Scanning file %s", file_path)
             with open(file_path, "rb") as file:
                 data = file.read()
-            issues_count = scan_bytes(data, file_path)
-            files_suspicious_count += 1 if issues_count > 0 else 0
-            files_scanned_count += 1
+            scan_result.merge(scan_bytes(data, file_path))
 
-    return files_scanned_count, files_suspicious_count
+    return scan_result
 
 
-def scan_file_path(path):
+def scan_file_path(path) -> ScanResult:
     with open(path, "rb") as file:
         data = file.read()
-    issues_count = scan_bytes(data, path)
-    return 1, 1 if issues_count > 0 else 0
+    return scan_bytes(data, path)
 
 
-def scan_url(url):
-    issues_count = scan_bytes(_http_get(url), url)
-    return 1, 1 if issues_count > 0 else 0
+def scan_url(url) -> ScanResult:
+    return scan_bytes(_http_get(url), url)
 
